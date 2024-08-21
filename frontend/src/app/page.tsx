ef89cac3b003209a8530f147f13803a7234d0888
franz1us <franzius.won@gmail.com>
2024-08-22 01:14:30 +0700

    start again
    start again

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="container mx-auto my-5">
+    <h1> Platform Practice</h1>
+    <hr className="my-3"/>
+    <div></div>
+    </main>
+  );
+}
